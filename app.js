686922903365e959e25ebe003ce36d16e52ee717
Vanessa Martin <vanessa@shopvida.com>
2019-04-22 13:53:25 -0700

    prepared for deploy

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 //LOAD ROUTES
 const users = require("./routes/api/users");
